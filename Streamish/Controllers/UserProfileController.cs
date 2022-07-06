382fd8f78e86b31fe2a437ae0aaf170781b2ffe0
jandershock <jordan.andershock@gmail.com>
2022-07-06 14:03:32 -0500

    Add userprofile implemented

---
+++ Streamish/Controllers/UserProfileController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Streamish.Models;
+using Streamish.Repositories;
+using System;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Streamish.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+        }
+
+        // GET: api/<UserProfileController>
+        [HttpGet]
+        public List<UserProfile> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/<UserProfileController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserProfileController>
+        [HttpPost]
+        public IActionResult Post(UserProfile userProfile)
+        {
+            _userProfileRepository.Add(userProfile);
+            return NoContent();
+        }
+
+        // PUT api/<UserProfileController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserProfileController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Streamish/Repositories/UserProfileRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Streamish.Models;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Streamish.Repositories
+{
+    public class UserProfileRepository : BaseRepository, IUserProfileRepository
+    {
+        public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
+        public void Add(UserProfile userProfile)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            INSERT INTO UserProfile ([Name], Email, ImageUrl, DateCreated)
+                            VALUES (@Name, @Email, @ImageUrl, @DateCreated)";
+                    foreach(PropertyInfo propertyInfo in userProfile.GetType().GetProperties())
+                    {
+                        if (propertyInfo.Name == "Id")
+                        {
+                            continue;
+                        }
+                        cmd.Parameters.AddWithValue($"@{propertyInfo.Name}", propertyInfo.GetValue(userProfile));
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public UserProfile Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<UserProfile> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(UserProfile userProfile)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
