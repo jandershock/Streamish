6c9b8a65de9b7bc1f8d3c8edc7feb9a56c086575
jandershock <jordan.andershock@gmail.com>
2022-07-07 16:15:52 -0500

    Created react app

---
+++ Streamish/client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import VideoList from "./components/VideoList.js";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoList />
+    </div>
+  );
+}
+
+export default App;
